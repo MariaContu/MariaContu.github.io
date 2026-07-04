a049b51745e7d9caec2dcf7a4cca59f6461b2638
MariaContu <mariaescontu@gmail.com>
2026-07-04 12:48:50 -0300

    feat: add skills and education sections

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,11 @@ import { Hero } from './components/hero/hero';
 import { About } from './components/about/about';
 import { Experience } from './components/experience/experience';
 import { Projects } from './components/projects/projects';
+import { SkillsEducation } from './components/skills-education/skills-education';
 
 @Component({
   selector: 'app-root',
-  imports: [Footer, Header, Hero, About, Experience, Projects],
+  imports: [Footer, Header, Hero, About, Experience, Projects, SkillsEducation],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
