da1bb5e29040908dd9f4f240f8e02e4ee422a90b
MariaContu <mariaescontu@gmail.com>
2026-07-04 11:11:46 -0300

    feat: add hero and about sections

---
+++ src/app/components/about/about.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SectionTitle } from "../section-title/section-title";
+
+@Component({
+  selector: 'app-about',
+  imports: [SectionTitle],
+  templateUrl: './about.html',
+  styleUrl: './about.css',
+})
+export class About {
+
+}
